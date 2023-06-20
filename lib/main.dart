76be0be96953d924f2b9143b757a5c2b9ed90d68
micckey <83111475+micckey@users.noreply.github.com>
2023-06-20 21:41:29 +0300

    onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,97 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'fair'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ClipPath(
-          clipper: CustomClipPath(),
-          child: Container(
-            width: double.infinity,
-            height: 400,
-            color: CustomColors().BackgroundColor,
-            child: Stack(children: <Widget>[
-              Positioned(
-                height: 250,
-                width: 400,
-                top: 5,
-                right: 5,
-                child: Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/icon.png'))),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: CustomColors().BackgroundColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 470,
+                  // color: Colors.pinkAccent,
+                  child: Stack(children: <Widget>[
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        height: 142,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+                          color: CustomColors().HighlightColor,
+                        ),
+                        child: const Center(
+                          child: Text('To help you keep track \n of techniques that need perfecting', style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ), textAlign: TextAlign.center),
+                        ),
+                      ),
+                    ),
+                    ClipPath(
+                      clipper: CustomClipPath(),
+                      child: Container(
+                        width: double.infinity,
+                        height: 400,
+                        color: CustomColors().CardColor,
+                        child: Stack(children: <Widget>[
+                          Positioned(
+                            height: 250,
+                            width: 400,
+                            top: 15,
+                            right: 5,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/images/icon.png'))),
+                            ),
+                          ),
+                          Positioned(
+                              child: Container(
+                                  margin: const EdgeInsets.only(top: 150),
+                                  child: const Center(
+                                      child: Text('DojoNotes', style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 60,
+                                      ),))))
+                        ]),
+                      ),
+                    ),
+
+                  ],),
                 ),
-              ),
-              Positioned(
-                  child: Container(
-                    margin: EdgeInsets.only(top: 150),
-                      child: Center(
-                          child: Text('DojoNotes'))))
-            ]),
+                const SizedBox(height: 20,),
+                const Text('Enter your Name below to proceed', style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold
+                ),),
+                // SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 224,
+                  height: 50,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(CustomColors().ButtonColor),
+                        shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                      ),
+                      child: Text('Get Started')),
+                )
+              ],
+            ),
           ),
         ),
       ),
